a8caa301d5c1b1ad06469c640ed4ff023da2e92c
valentinaandrade <valentinaandrade@uchile.cl>
2021-08-12 12:51:34 -0400

    add sintaxis uah

--- R/02analysis-uah.R
+++ R/02analysis-uah.R
@@ -59,4 +59,31 @@ uah %>% select(1:number_reporte) %>%
   guides(fill = F) +
   scale_fill_futurama()
 
-  
+# Aprendi -----------------------------------------------------------------
+aprendi <- uah %>% select(1:number_reporte) %>% 
+  pivot_longer(cols =c(aprendi_1:aprendi_5), names_to = "number_mention", values_to = "aprendi") %>%
+  filter(!is.na(aprendi)) %>% 
+  group_by(aprendi) %>% 
+  summarise(n = n()*10) %>%
+  arrange(-n) %>% 
+  ungroup() %>%
+  mutate(aprendi1 = aprendi) %>% 
+  column_to_rownames(var = "aprendi1")
+
+set.seed(186353609)
+
+wordcloud2::wordcloud2(aprendi)
+
+uah %>% select(1:number_reporte) %>% 
+  pivot_longer(cols =c(aprendi_1:aprendi_5), names_to = "number_mention", values_to = "aprendi") %>%
+  filter(!is.na(aprendi)) %>% 
+  group_by(aprendi) %>% 
+  summarise(n = n()) %>% 
+  mutate(prop = n/sum(n)*100) %>% 
+  ggplot(aes(x = reorder(aprendi, -prop), y = prop, fill = aprendi)) +
+  geom_bar(stat = "identity", color = "black") +
+  geom_text(aes(label = paste0(round(prop,1), "%")), vjust=-0.25) +
+  labs(x = "", y = "%", title = "Contenidos preferentes a estudiar en el curso", subtitle = "Respuesta de selección múltiple") + 
+  guides(fill = F) +
+  scale_fill_futurama()
+
